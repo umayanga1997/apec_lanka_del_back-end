432667fc329f220dcd719437cc64bf37329859c9
umayanga1997 <umayanga.avi@gmail.com>
2021-03-11 22:24:29 +0530

    ...Updates-06

--- Controllers/algolia.js
+++ Controllers/algolia.js
@@ -8,7 +8,7 @@ async function pushItemsToAlgolia(city) {
     const client = algoliasearch(process.env.Al_Application_ID, process.env.Al_Admin_API_Key);
     const index = client.initIndex(`${city}_items`);
 
-    const response = await pool.query("SELECT * FROM items").then((result) => {
+    const response = await pool.query("select  items.* , CAST(avg(CASE when ir.rate_value != 0 then  ir.rate_value else 0 end) as decimal(2,1)) as rate from items full outer join  item_rates ir on items.item_id = ir.item_id group by items.item_count").then((result) => {
       var newIndexes = [];
 
       for (const key in result.rows) {
@@ -21,10 +21,10 @@ async function pushItemsToAlgolia(city) {
         .then(({
           // objectIDs
         }) => {
-          console.log(objectIDs);
+          // console.log(objectIDs);
         })
         .catch(err => {
-          console.log(err);
+          // console.log(err);
         });
     });
   } catch (error) {
