0c55d3741280e42550d599cc29625294ac2749b8
umayanga1997 <umayanga.avi@gmail.com>
2021-03-17 13:29:22 +0530

    ...Updates-19

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/view/', function (req, res, next) {
- res.render('index', {title:"Hello World"});
-})
-
-module.exports = router;
