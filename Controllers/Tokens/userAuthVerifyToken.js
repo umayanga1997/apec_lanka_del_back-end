86c095eeae515bcea8ef6470001c111551f9a6d6
umayanga1997 <umayanga.avi@gmail.com>
2021-03-20 19:55:58 +0530

    ...Updates-25

--- Controllers/Tokens/userAuthVerifyToken.js
+++ Controllers/Tokens/userAuthVerifyToken.js
@@ -1,14 +1,27 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
-     const token = req.header('auth-token');
-     if(!token) return res.status(401).send("Access Denied");
+module.exports = async function (req, res, next) {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).send("Access Denied");
 
-     try {
-         const verify = jwt.verify(token, process.env.TOKEN_SECRET);
-         req.userVerify = verify;
-         next();
-     } catch (error) {
+    try {
+        const verify = jwt.verify(token, process.env.TOKEN_SECRET);
+        const response = await pool.query("SELECT * FROM users WHERE user_id=$1", [verify._id.user_id]);
+        if (response.rowCount != 0 && response.rowCount != null) {
+            if (res.status(200)) {
+                req.userVerify = verify;
+                next();
+            } else {
+                res.json({
+                    done: false,
+                    message: "Has some issue(s) with status, Try again.",
+                    data: []
+                })
+            }
+        } else {
+            res.status(401).send("Unauthorized");
+        }
+    } catch (error) {
         res.status(400).send("Invalid Token");
-     }
+    }
 }
